c554b1eba8b7d627c247525f1579d7a6c61f3cc2
SinaAgh <sinaagh2@gmail.com>
2022-08-04 20:52:32 +0430

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+let view = document.getElementById('pageView');
+let cost = document.getElementById('cost');
+let cost2 = document.getElementById('cost2');
+let slider = document.getElementById('price');
+let toggle = document.getElementById('billing');
+let pageView = ["10K", "50K", "100K", "500", "1M"];
+let perMonth = [8, 12, 16, 24, 36];
+let isYearly = false;
+
+slider.addEventListener('input', function () {
+    updateValue();
+    view.innerHTML = pageView[slider.value]
+
+    value = this.value * 25
+
+    this.style.background = `linear-gradient(to right,
+        hsl(174, 77%, 80%) 0%,
+        hsl(174, 77%, 80%) ${value}%,
+        hsl(224, 65%, 95%) 0%,
+        hsl(224, 65%, 95%) 100%)`
+
+
+});
+
+toggle.addEventListener('change', function () {
+    if (isYearly === false) {
+        isYearly = true
+    } else {
+        isYearly = false;
+    }
+    updateValue()
+});
+
+
+function updateValue() {
+    if (isYearly) {
+        cost.innerHTML = perMonth[slider.value] * 75
+        cost2.innerHTML = perMonth[slider.value] * 75
+    } else {
+        cost.innerHTML = perMonth[slider.value]
+        cost2.innerHTML = perMonth[slider.value]
+    }
+}
